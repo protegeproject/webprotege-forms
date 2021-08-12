00347f21f0da08795b53cdd317f5385556658a4c
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-11 17:48:46 -0700

    Initial skeleton

---
+++ src/main/java/edu/stanford/protege/webprotege/forms/WebprotegeFormsApiApplication.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.forms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprotegeFormsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprotegeFormsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/stanford/protege/webprotege/forms/WebprotegeFormsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprotegeFormsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
